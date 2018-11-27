f256b2faa60a6a9af65c0bc2daa30f4dc10c5771
Louis Thievent <louis.thievent@gmail.com>
2018-11-27 11:10:00 +0100

    creater notifier

---
+++ app/src/main/java/fr/wildcodeschool/mediaplayer/Notifier.java
@@ -0,0 +1,31 @@
+package fr.wildcodeschool.mediaplayer;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+
+public class Notifier extends Builder {
+    private String title;
+    private String artist;
+
+    public Notifier(@NonNull Context context, @NonNull String channelId, String title, String artist) {
+        super(context, channelId);
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public void setup()
+    {
+        this.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.ic_music)
+            // Add media control buttons that invoke intents in your media service
+            .addAction(R.drawable.ic_play, "Play", null) // #0
+            .addAction(R.drawable.ic_pause, "Pause", null)  // #1
+            .addAction(R.drawable.ic_stop, "Stop", null)     // #2
+            // Apply the media style template
+            .setContentTitle(this.title)
+            .setContentText(this.artist)
+            .build();
+    }
+}
